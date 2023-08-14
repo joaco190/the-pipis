d6bebafc7957546b76123fe7f21631d1848fa046
joaco190 <joaco190@gmail.com>
2023-08-14 02:21:47 -0300

    cambio de texto en boton, vermas/vermenos

--- src/componentes/productos.js
+++ src/componentes/productos.js
@@ -5,15 +5,18 @@ export default class Productos extends React.Component{
     constructor(props){
         super(props);
         this.state= {
-            escondido: true
-        }
+            escondido: true,
+            buttonValue: "Ver más"
+        };
 
         this.toggleEscondido = this.toggleEscondido.bind(this);
+
     }
 
     toggleEscondido(){
         this.setState(prevState =>({
-            escondido: !prevState.escondido
+            escondido: !prevState.escondido,
+            buttonValue: prevState.escondido ? "Ver menos" : "Ver más"
         }))
     }
     render(){
@@ -23,7 +26,7 @@ export default class Productos extends React.Component{
             <div className="card-body">
                 <h5 classsName="card-title">{this.props.nomProducto} </h5>
                 <p className="card-text">{this.props.descriProducto}</p>
-                <button className="btn btn-danger" onClick={this.toggleEscondido}>Ver más</button>
+                <button className="btn btn-danger" onClick={this.toggleEscondido}>{this.state.buttonValue}</button>
                 <p class={"breakMyWords card card-text m-2 bg-warning-subtle " +(this.state.escondido ? " d-none" : "")}>{this.props.compleProducto}</p>
             </div>
             
